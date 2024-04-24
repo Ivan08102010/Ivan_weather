b404050fcabdfb39bb7359985d85a5ff9c93772b
Ivan <ivanabasin2@gmail.com>
2024-04-24 19:46:04 +0300

    ОЧЕНЬ МНОГО ЧЕГО ДОБАВЛЕННО

--- Ivan_weather/weather/s.py
+++ Ivan_weather/weather/s.py
@@ -1,68 +1,3 @@
-# import asyncio
-# import time
-# from pprint import pprint
-#
-# from aiohttp import ClientSession
-#
-#
-# async def get_weather(city):
-#     with ClientSession() as session:
-#         url = f'http://api.openweathermap.org/data/2.5/weather'
-#         params = {'q': city, 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
-#
-#         async with session.get(url=url, params=params) as response:
-#             weather_json = await response.json()
-#             v = weather_json["main"]["temp"] - 273.15
-#             return v
-#
-#
-# async def main(cities_):
-#     tasks = []
-#     for city in cities_:
-#         tasks.append(asyncio.create_task(get_weather(city)))
-#
-#     for task in tasks:
-#         await task
-#
-#
-# cities = ['Bryansk']
-# if __name__ == '__main__':
-#     asyncio.run(main(cities))
-#
-# import asyncio
-# import time
-# from aiohttp import ClientSession
-#
-#
-# async def get_weather(city):
-#     async with ClientSession() as session:
-#         url = f'http://api.openweathermap.org/data/2.5/weather'
-#         params = {'q': city, 'APPID': '17bc5822537ddc276d62eb5191385a58'}
-#
-#         async with session.get(url=url, params=params) as response:
-#             weather_json = await response.json()
-#             v = weather_json["main"]["temp"] - 273.15
-#             return str(int(v))
-#
-#
-# async def main(cities_):
-#     tasks = []
-#     for city in cities_:
-#         tasks.append(asyncio.create_task(get_weather(city)))
-#
-#     results = await asyncio.gather(*tasks)
-#
-#     for result in results:
-#         print(result)
-#
-#
-#
-#
-# cities = ['Bryansk']
-# if __name__ == '__main__':
-#     asyncio.run(main(cities))
-
-
 import requests
 
 
@@ -74,3 +9,12 @@ def get_weather(city):
     #print(weather_json)
     v = weather_json["main"]["temp"] - 273.15
     return str(int(v))
+
+
+def get_weather_2(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather'
+    params = {'q': city, 'APPID': '2a4ff86f9aaa70041ec8e82db64abf56'}
+
+    weather_json = requests.get(url=url, params=params).json()
+    return (f'{weather_json["weather"][0]["main"]}')
+    print  (f'{weather_json["weather"][0]["main"]}')
--- Ivan_weather/weather/views.py
+++ Ivan_weather/weather/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-from weather.s import get_weather
+from weather.s import get_weather,get_weather_2
+b = '0'
 
 menu = [{'title': 'Главная', 'url_n': 'first'},
 
@@ -13,5 +14,9 @@ menu = [{'title': 'Главная', 'url_n': 'first'},
 
 def first(request):
     v = get_weather('Bryansk')
-    print(v)
-    return render(request, "weather/first.html", context={'menu': menu, 'v': v})
+    b = int(v)
+    w = get_weather_2('Bryansk')
+
+    return render(request, "weather/first.html", context={'menu': menu, 'v': v, 'b' : b,'w':w})
+
+
