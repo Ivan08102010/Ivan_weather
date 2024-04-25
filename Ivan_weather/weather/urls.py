0691a27ed5832372a52eae257ce2a3ce521f4a91
Ivan <ivanabasin2@gmail.com>
2024-04-25 10:07:45 +0300

    Доделана погода в брянске (25.04.24)

--- Ivan_weather/weather/urls.py
+++ Ivan_weather/weather/urls.py
@@ -3,4 +3,6 @@ from weather.views import *
 from weather import views
 urlpatterns = [
     path('',first,name = 'first'),
+    path('',city_base,name = 'city_b'),
+    path('city/<int:city_id>/', citis, name='city'),
 ]
--- Ivan_weather/weather/views.py
+++ Ivan_weather/weather/views.py
@@ -7,7 +7,7 @@ from weather.s import get_weather,get_weather_2
 b = '0'
 
 menu = [{'title': 'Главная', 'url_n': 'first'},
-
+        {'title': 'Города', 'url_n': 'city_b'}
         ]
 
 
@@ -18,5 +18,18 @@ def first(request):
     w = get_weather_2('Bryansk')
 
     return render(request, "weather/first.html", context={'menu': menu, 'v': v, 'b' : b,'w':w})
+
+def city_base(request,city_id):
+    v = get_weather('Bryansk')
+    b = int(v)
+    return render(request, "weather/city_base.html", context={'menu': menu, 'b' : b,})
+
+def citis(request,city_id):
+    if city_id == 1:
+        v = get_weather('Moscow')
+        b = int(v)
+        w = get_weather_2('Moscow')
+        return render(request, "weather/first.html", context={'menu': menu, 'v': v, 'b' : b,'w':w})
+
 
 
