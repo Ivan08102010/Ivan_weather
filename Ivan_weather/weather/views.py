5982f6aa61f85845beec571c56ed74e75e580d22
Ivan <ivanabasin2@gmail.com>
2024-05-16 19:46:07 +0300

    Всё готово. Добавлен CSS (16.05.24;(19:46))

--- Ivan_weather/weather/views.py
+++ Ivan_weather/weather/views.py
@@ -13,7 +13,6 @@ def first(request):
     v = get_weather('Bryansk')
     b = int(v)
     w = get_weather_2('Bryansk')
-
     return render(request, "weather/first.html", context={'menu': menu, 'v': v, 'b' : b,'w':w})
 
 def city_base(request):
@@ -31,6 +30,7 @@ def city_base(request):
         v = '0'
         b = 0
         w = '0'
+
 
     return render(request, "weather/city_base.html", context={'menu': menu, 'b' : b,'w':w,"vvod":vvod,'city':city})
 
